f2bf50ebe993d4339dbc9542f2905b2d57015d30
Gomah <stivinrotovegas@gmail.com>
2017-07-17 15:26:00 +0000

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -2,8 +2,9 @@
 from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
-
-
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('css', path)
 
 @app.route('/img/<path:path>')
 def send_js(path):
